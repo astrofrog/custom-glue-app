72f3331e16d2713af1814e6ac946774cde2bf735
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-10-03 17:15:55 +0200

    Basic functionality implemented. It’s still pretty ugly!

--- pv_slicer.py
+++ pv_slicer.py
@@ -8,6 +8,7 @@ from PyQt4.uic import loadUi
 from glue.qt.widgets.image_widget import ImageWidget, PVSliceWidget
 from glue.qt.qtutil import data_wizard
 
+
 class PVSlicer(Application, QMainWindow):
     """
     A proof-of-concept position-velocity slicer using glue
@@ -24,24 +25,29 @@ class PVSlicer(Application, QMainWindow):
 
         self.ui = loadUi('slicer.ui', None)
         self.setCentralWidget(self.ui)
-        self.resize(1024, 768)
+        self.resize(1200, 800)
 
-        w1 = ImageWidget(session=self._session)
+        self.image = ImageWidget(session=self._session)
         image = np.random.random((12,12))
         x = np.linspace(-5., 5., 12)
         y = np.linspace(-5., 5., 12)
 
-        w2 = PVSliceWidget(image, x, y, w1)
+        self.slice = PVSliceWidget(image, x, y, self.image)
 
-        self.ui.main_layout.addWidget(w1)
-        self.ui.main_layout.addWidget(w2)
+        self.image._slice_widget = self.slice
 
-        self.im = w1
+        self.ui.main_layout.addWidget(self.image)
+        self.ui.main_layout.addWidget(self.slice)
 
         self.ui.load_button.clicked.connect(self._load_data)
 
-    def _load_settings(self):
-        pass
+    def _load_data(self):
+
+        for data in list(self.data_collection):
+            self.data_collection.remove(data)
+
+        self.add_datasets(self.data_collection, data_wizard())
+        self.image.add_data(self.data_collection[0])
 
     def start(self):
         """
@@ -53,15 +59,17 @@ class PVSlicer(Application, QMainWindow):
 
     exec_ = start
 
+    # TODO: the following are needed to run, but don't do anything useful. Make
+    # it so that they are not required.
+
     def add_widget(self, new_widget, label=None, tab=None,
                    hold_position=False):
         sub = new_widget.mdi_wrap()
         return sub
 
-    def _load_data(self):
-        self.add_datasets(self.data_collection, data_wizard())
-        self.im.client.set_data(self.data_collection[0])
-        self.im.client._redraw()
+    def _load_settings(self):
+        pass
+
 
 
 
